9ca6d64810e2bf549f0eb670b1e96855efb79665
Spelchure <alperenbayramoglu2@gmail.com>
2022-06-26 12:01:48 +0300

    Express error handling middleware & database connection.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
-import express, { Request, Response } from "express";
-import bodyParser from "body-parser";
+import express, {NextFunction, Request, Response} from 'express';
+import cors from 'cors';
 
 export default function createServer() {
   const app = express();
 
-  //app.use(bodyParser.json());
-  //app.use(cors());
-  app.use("/", (req: Request, res: Response) => {
-    res.status(200).json({ message: "Success" });
+  app.use(cors());
+
+  app.use('/', (_: Request, res: Response) => {
+    res.status(200).json({message: 'Success'});
   });
 
   /* Error handler */
-  app.use((error: Error, req: Request, res: Response) => {
-    return res.status(500).json({ error: error.message });
+  // eslint-disable-next-line
+  app.use((error: Error, req: Request, res: Response, _: NextFunction) => {
+    return res.status(500).json({error: error.message});
   });
   return app;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
-import createExpressApp from "./app";
+import createExpressApp from './app';
+import mongoose from 'mongoose';
 
 const startServer = async () => {
   const port = +(process.env.PORT || 8082);
   const app = createExpressApp();
 
   try {
+    await mongoose.connect(<string>process.env.MONGODB_URL);
+
     app.listen(port);
     console.log(`Server is listening ${port}`);
   } catch (error) {
